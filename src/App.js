3fdb7c8cbb7295b2e13f397993436822e6105db2
Sean <sbernath@fullsail.com>
2022-05-26 20:08:55 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./Counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={styleStuff}>
+      <Counter color="Red" interval={5} />
+      <Counter color="Green" interval={5} />
+      <Counter color="Blue" interval={5} />
+      <Counter color="Yellow" interval={5} />
     </div>
   );
 }
-
+const styleStuff = {
+  display: "flex",
+  flexDirection:"row",
+  background:'black',
+  justifyContent:"space-evenly"
+}
 export default App;
